6d5a8bc04ccc61e75b0289b2b79a5e7894720d51
Atunde Toheeb Ayomide <atundetoheeb1@gmail.com>
2025-08-27 01:39:52 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Expense-Tracker-Web-App
+# 💸 Expense Tracker
+
+A simple expense tracker implemented in Python (CLI) and Node.js (Web App).  
+It helps record expenses, list them, calculate totals, and export to CSV.
+
+---
+![Expenses Tracker Demo](public/images/screenshot.png)
+
+## 🚀 Features
+- Add expenses with description, amount, category, and timestamp
+- List expenses in table format
+- Calculate total expenses
+- Export expenses to CSV
+- Node.js version includes a web UI with HTML + CSS
+
+---
+
+## 🐍 Python Version
+Run:
+
+```bash
+python expense_tracker.py
---
+++ index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import bodyParser from "body-parser";
+import fs from "fs";
+import { timeStamp } from "console";
+
+const app = express();
+const port = 3000;
+
+let expenses = [];
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static("public"));
+
+app.post("/add", (req, res) => {
+    const { description, amount, category } = req.body;
+    const expense = {
+        description,
+        amount: parseFloat(amount).toFixed(2),
+        category: category || "General",
+        timestamp: new Date().toLocaleString()
+    };
+    expenses.push(expense);
+    res.redirect("/");
+});
+
+app.get("/expenses", (req, res) => {
+    res.json(expenses);
+});
+
+// export to csv
+app.get("/export", (req, res) => {
+    const header = "Description, Amount, Category, Timestamp\n";
+    const rows = expenses.map(exp => `${exp.description},${exp.amount},${exp.category},${exp.timestamp}`).join("\n");
+    fs.writeFileSync("expenses.csv", header + rows);
+    res.download("expenses.csv");
+});
+
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`)
+})
